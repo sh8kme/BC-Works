e918cfcf7e8d7de99f10c3f4d6ba47a8044391a8
sh8kme <starrje@hotmail.com>
2020-09-18 08:56:23 -0700

    Update and rename autoview47.js to autoview48.js

--- autoview48.js
+++ autoview48.js
@@ -198,7 +198,19 @@ setTimeout(check, 7000);
 
 function AV1() {
  $('#marker-22-undefined')[0].click();
-	$('.mapboxgl-popup-content').append('<pre id="typewriter"><span>asdasdasd</span></pre>')
+	
+	var content = 'If life is so blue, then, select another colour from thee rainbow';
+	var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('.mapboxgl-popup-content').each(function (i) {
+    $(this).delay(50 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
  var timer = setTimeout(function() {
 AV2();
  	  }, 7000);
@@ -369,73 +381,4 @@ $(document).ready(function() {
     });
 });
 
-function setupTypewriter(t) {
-	    var HTML = t.innerHTML;
 
-	    t.innerHTML = "";
-
-	    var cursorPosition = 0,
-	        tag = "",
-	        writingTag = false,
-	        tagOpen = false,
-	        typeSpeed = 100,
-        tempTypeSpeed = 0;
-
-	    var type = function() {
-        
-	        if (writingTag === true) {
-	            tag += HTML[cursorPosition];
-	        }
-
-	        if (HTML[cursorPosition] === "<") {
-	            tempTypeSpeed = 0;
-	            if (tagOpen) {
-	                tagOpen = false;
-	                writingTag = true;
-	            } else {
-	                tag = "";
-	                tagOpen = true;
-	                writingTag = true;
-	                tag += HTML[cursorPosition];
-	            }
-	        }
-	        if (!writingTag && tagOpen) {
-	            tag.innerHTML += HTML[cursorPosition];
-	        }
-	        if (!writingTag && !tagOpen) {
-	            if (HTML[cursorPosition] === " ") {
-	                tempTypeSpeed = 0;
-	            }
-	            else {
-	                tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-	            }
-	            t.innerHTML += HTML[cursorPosition];
-	        }
-	        if (writingTag === true && HTML[cursorPosition] === ">") {
-	            tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-	            writingTag = false;
-	            if (tagOpen) {
-	                var newSpan = document.createElement("span");
-	                t.appendChild(newSpan);
-	                newSpan.innerHTML = tag;
-	                tag = newSpan.firstChild;
-	            }
-	        }
-
-	        cursorPosition += 1;
-	        if (cursorPosition < HTML.length - 1) {
-	            setTimeout(type, tempTypeSpeed);
-	        }
-
-	    };
-
-	    return {
-	        type: type
-	    };
-	}
-
-	var typer = document.getElementById('typewriter');
-
-	typewriter = setupTypewriter(typewriter);
-
-	typewriter.type();
