9b3b39eba601f967c8b19067c9f4ba1ee125946f
sh8kme <starrje@hotmail.com>
2020-09-18 09:55:55 -0700

    Update and rename autoview60.js to autoview61.js

--- autoview61.js
+++ autoview61.js
@@ -239,7 +239,22 @@ AV2();
 function AV2() {
 	$( ".close" ).unbind();
  $('#marker-24-undefined')[0].click();
-	
+	var content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.';
+	var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+setTimeout(
+  function() 
+  {
+$('<h6 class="typer">').appendTo('.mapboxgl-popup-content');
+	 
+$(ele).hide().appendTo('.typer').each(function (i) {
+    $(this).delay(50 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
+ }, 1500);  	
  var timer = setTimeout(function() {
 AV3();
  	  }, 7000);
